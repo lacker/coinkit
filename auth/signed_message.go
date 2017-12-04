81b8830321f9468be0a4ed27ba59f9a5e3927937
Kevin Lacker <lacker@gmail.com>
2017-12-04 09:50:32 -0800

    signed message tools

--- auth/signed_message.go
+++ auth/signed_message.go
@@ -1,5 +1,9 @@
 package auth
 
+import (
+	"errors"
+)
+
 type SignedMessage struct {
 	message string
 	signer string
@@ -16,4 +20,19 @@ func NewSignedMessage(kp *KeyPair, message string) *SignedMessage {
 
 func (sm *SignedMessage) Serialize() string {
 	return fmt.Sprintf("e:%s:%s:%s", sm.signer, sm.signature, sm.message)
+}
+
+func NewSignedMessageFromSerialized(serialized string) (*SignedMessage, err error) {
+	parts := string.SplitN(serialized, ":", 4)
+	if len(parts) != 4 {
+		return nil, errors.New("could not find 4 parts")
+	}
+	version, signer, signature, message := parts
+	if version != "e" {
+		return nil, errors.New("unrecognized version")
+	}
+	if !Verify(signer, message, signature) {
+		return nil, errors.New("signature failed verification")
+	}
+	return &SignedMessage{message: message, signer: signer, signature: signature}
 }
