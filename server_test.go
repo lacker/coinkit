318aa3d7407be471336f0b0cbe7c84cc93d15ec4
Kevin Lacker <lacker@gmail.com>
2017-12-17 10:46:03 -0800

    super basic network testing

---
+++ server_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestBasicNetwork(t *testing.T) {
+	c0 := NewLocalConfig(0)
+	c1 := NewLocalConfig(1)
+	c2 := NewLocalConfig(2)
+	s0 := NewServer(c0)
+	s1 := NewServer(c1)
+	s2 := NewServer(c2)
+	go s0.ServeForever()
+	go s1.ServeForever()
+	go s2.ServeForever()
+}
