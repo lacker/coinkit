6ea8f9eb64cfd870d5589a125148d5d9e272318e
Kevin Lacker <lacker@gmail.com>
2018-01-27 14:48:32 -0800

    more bits of cli

--- cmd/cclient/main.go
+++ cmd/cclient/main.go
@@ -33,16 +33,29 @@ func status(user string) {
 	log.Printf("response: %s", spew.Sdump(m))
 }
 
+// Ask the user for a passphrase to log in.
+func login() *util.KeyPair {
+	panic("TODO")
+}
+
 // cclient runs a client that connects to the coinkit network.
 func main() {
 	if len(os.Args) < 3 {
-		log.Fatal("Usage: cclient status <publickey>")
+		log.Fatal("Usage: cclient {send,status} ...")
 	}
 	op := os.Args[1]
 	rest := os.Args[2:]
 	switch op {
 	case "status":
+		if len(rest) != 1 {
+			log.Fatal("Usage: cclient status <publickey>")
+		}
 		status(rest[0])
+	case "send":
+		if len(rest) != 2 {
+			log.Fatal("Usage: cclient send <user> <amount>")
+		}
+		panic("TODO")
 	default:
 		log.Fatalf("unrecognized operation: %s", op)
 	}
