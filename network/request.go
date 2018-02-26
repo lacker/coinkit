f3468c41581e820c3bcd76e800d698c0e02a6712
Kevin Lacker <lacker@gmail.com>
2018-02-26 11:05:23 -0800

    remove line functionality of requests

--- network/request.go
+++ network/request.go
@@ -12,18 +12,7 @@ import (
 type Request struct {
 	Message *util.SignedMessage
 
-	// If Message is nil, it has been pre-encoded into MessageString.
-	Line string
-
 	Response chan *util.SignedMessage
 
 	Timeout time.Duration
-}
-
-func (r *Request) GetLine() string {
-	if r.Message != nil && len(r.Line) == 0 {
-		// We need to calculate the message string
-		r.Line = util.SignedMessageToLine(r.Message)
-	}
-	return r.Line
 }
