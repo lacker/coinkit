cec9f6eb3b4591ef0a0b942e5e378c4da825047c
Kevin Lacker <lacker@gmail.com>
2018-02-28 15:07:18 -0800

    lets get raw wth postgres

--- data/database.go
+++ data/database.go
@@ -1,15 +1,16 @@
 package data
 
 import (
+	"fmt"
 	"os/user"
 
-	"github.com/go-pg/pg"
-	"github.com/go-pg/pg/orm"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 // A Database encapsulates a connection to a Postgres database.
 type Database struct {
-	postgres *pg.DB
+	postgres *sqlx.DB
 }
 
 // Creates a new database handle designed to be used for unit tests.
@@ -18,27 +19,48 @@ func NewTestDatabase() *Database {
 	if err != nil {
 		panic(err)
 	}
+	postgres := sqlx.MustConnect(
+		"postgres", fmt.Sprintf("user=%s dbname=test sslmode=disable", user.Username))
+
 	db := &Database{
-		postgres: pg.Connect(&pg.Options{
-			User:     user.Username,
-			Password: "",
-			Database: "test",
-		}),
+		postgres: postgres,
 	}
 	db.initialize()
 	return db
 }
 
-func (db *Database) createTable(obj interface{}) {
-	err := db.postgres.CreateTable(obj, &orm.CreateTableOptions{
-		IfNotExists: true,
-	})
+const schema = `
+CREATE TABLE IF NOT EXISTS blocks (
+    slot integer,
+    value text,
+    c integer,
+    h integer
+)
+`
+
+const blockInsert = `
+INSERT INTO blocks (slot, value, c, h)
+VALUES (:slot, :value, :c, :h)
+`
+
+// initialize makes sure the schemas are set up right and panics if not
+func (db *Database) initialize() {
+	db.postgres.MustExec(schema)
+}
+
+func (db *Database) SaveBlock(b *Block) {
+	_, err := db.postgres.NamedExec(blockInsert, b)
 	if err != nil {
 		panic(err)
 	}
 }
 
-// initialize makes sure the schemas are set up right and panics if not
-func (db *Database) initialize() {
-	db.createTable(&Block{})
+// GetBlock returns nil if there is no block for the provided slot.
+func (db *Database) GetBlock(slot int) *Block {
+	answer := &Block{}
+	err := db.postgres.Get(answer, "SELECT * FROM blocks WHERE slot=$1", slot)
+	if err != nil {
+		panic(err)
+	}
+	return answer
 }
--- data/database_test.go
+++ data/database_test.go
@@ -4,6 +4,23 @@ import (
 	"testing"
 )
 
-func TestDatabaseCreation(t *testing.T) {
-	NewTestDatabase()
+func TestSaveAndFetch(t *testing.T) {
+	db := NewTestDatabase()
+	block := &Block{
+		Slot:  3,
+		Value: "foo",
+		C:     3,
+		H:     4,
+	}
+	db.SaveBlock(block)
+	b2 := db.GetBlock(3)
+	if b2.C != block.C {
+		t.Fatal("block changed: %+v -> %+v", block, b2)
+	}
 }
+
+// TODO: test that:
+// saving and fetching works ok
+// fetching a nonexistent block does not die
+// block slots are enforced to be unique
+// lastblock works
