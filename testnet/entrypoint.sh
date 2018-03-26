e0665176817af6815ae9d36fdd8dead6b2c76ea6
Kevin Lacker <lacker@gmail.com>
2018-03-26 10:05:00 -0700

    move entry point to its own shell file

---
+++ testnet/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is the entry point for the Docker container, designed to be run on
+# the Google cloud platform from the coinkit directory.
+
+cserver \
+    --keypair=./testnet/keypair0.json \
+    --network=./testnet/network.json \
+    --http=8000 \
+    --project=$PROJECT_ID
