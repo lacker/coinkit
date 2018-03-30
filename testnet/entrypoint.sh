4e60bc445bd0ebf329c4c6fc95b77def5aa1ba7d
Kevin Lacker <lacker@gmail.com>
2018-03-30 15:08:03 -0700

    fix keypair loading

--- testnet/entrypoint.sh
+++ testnet/entrypoint.sh
@@ -3,8 +3,12 @@
 # This script is the entry point for the Docker container, designed to be run on
 # the Google cloud platform from the coinkit directory.
 
+echo "contents of /secrets/keypair:"
+ls /secrets/keypair
+KEYPAIR=`ls /secrets/keypair | grep json | head -1`
+
 cserver \
-    --keypair=/secrets/keypair.json \
+    --keypair=$KEYPAIR \
     --network=./testnet/network.json \
     --logtostdout \
     --http=8000
