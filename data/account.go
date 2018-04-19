f947a2a11ff50eb80f42ef86282d6333cfda3522
Kevin Lacker <lacker@gmail.com>
2018-04-18 17:02:37 -0700

    Account iterator

--- data/account.go
+++ data/account.go
@@ -32,6 +32,27 @@ func StringifyAccount(a *Account) string {
 	return fmt.Sprintf("%s:s%d:b%d", util.Shorten(a.Owner), a.Sequence, a.Balance)
 }
 
+func (a *Account) CheckEqual(other *Account) error {
+	if a == nil && other == nil {
+		return nil
+	}
+	if a != nil || other != nil {
+		return fmt.Errorf("a != other. a is %+v, other is %+v", a, other)
+	}
+	if a.Owner != other.Owner {
+		return fmt.Errorf("owner %s != owner %s", a.Owner, other.Owner)
+	}
+	if a.Sequence != other.Sequence {
+		return fmt.Errorf("data mismatch for owner %s: seq %d != seq %d",
+			a.Owner, a.Sequence, other.Sequence)
+	}
+	if a.Balance != other.Balance {
+		return fmt.Errorf("data mismatch for owner %s: balance % != balance %d",
+			a.Owner, a.Balance, other.Balance)
+	}
+	return nil
+}
+
 func (a *Account) Bytes() []byte {
 	return []byte(fmt.Sprintf("%s:%d:%d", a.Owner, a.Sequence, a.Balance))
 }
