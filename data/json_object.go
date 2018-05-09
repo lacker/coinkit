e7a475bc23d0220a82f9f02a4fa02f6a07a026f1
Kevin Lacker <lacker@gmail.com>
2018-05-09 15:11:02 -0700

    JSONObject

---
+++ data/json_object.go
@@ -0,0 +1,10 @@
+package data
+
+import ()
+
+// JSONObject is a modifiable set of key-value mappings.
+// It is designed to be more convenient than representing JSON as either a
+// map[string]interface{} or the raw bytes.
+type JSONObject struct {
+	// TODO: implement
+}
