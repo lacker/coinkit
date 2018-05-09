4afb11330fab0f85f69d95100d54fad0d8dad329
Kevin Lacker <lacker@gmail.com>
2018-05-09 15:17:32 -0700

    NewJSONObject

--- data/json_object.go
+++ data/json_object.go
@@ -1,10 +1,30 @@
 package data
 
-import ()
+import (
+	"encoding/json"
+)
 
 // JSONObject is a modifiable set of key-value mappings.
 // It is designed to be more convenient than representing JSON as either a
 // map[string]interface{} or the raw bytes.
 type JSONObject struct {
-	// TODO: implement
+	bytes   []byte
+	content map[string]interface{}
+}
+
+// Sets bytes based on content
+func (ob *JSONObject) encode() {
+	bytes, err := json.Marshal(ob.content)
+	if err != nil {
+		panic(err)
+	}
+	ob.bytes = bytes
+}
+
+func NewJSONObject() *JSONObject {
+	answer := &JSONObject{
+		content: make(map[string]interface{}),
+	}
+	answer.encode()
+	return answer
 }
