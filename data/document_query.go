d98b4a631fb2ac4adce0dcb60988200ed3da61fd
Kevin Lacker <lacker@gmail.com>
2018-07-12 10:18:08 -0700

    limit in dq

--- data/document_query.go
+++ data/document_query.go
@@ -7,4 +7,7 @@ type DocumentQuery struct {
 	// Each field in data expresses an exact match with the data of the object to
 	// be retrieved.
 	Data *JSONObject
+
+	// The maximum number of objects to be returned.
+	Limit int
 }
