b7a4fbd657b4c502407f52848da06ef9734fc208
Kevin Lacker <lacker@gmail.com>
2018-08-07 10:44:01 -0700

    fix log

--- data/database_test.go
+++ data/database_test.go
@@ -154,7 +154,7 @@ func TestGetDocuments(t *testing.T) {
 	}
 	docs, slot := db.GetDocuments(map[string]interface{}{"a": 2, "b": 1}, 2)
 	if slot != 0 {
-		t.Fatalf("wrong slot: %d")
+		t.Fatalf("wrong slot: %d", slot)
 	}
 	if len(docs) != 0 {
 		t.Fatal("expected no docs visible before commit")
