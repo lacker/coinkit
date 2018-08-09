d5e73a3ff9bd5f7c6e5a719499f259ef0cd9a21b
Kevin Lacker <lacker@gmail.com>
2018-08-09 12:06:34 -0700

    nicer msg

--- js/src/main.js
+++ js/src/main.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 window.onload = () => {
-  const element = <h1>this is React</h1>;
+  const element = <h1>hello world</h1>;
   ReactDOM.render(element, document.getElementById("root"));
 };
