a459670361a0b2e393d86e07e5902eed8008fc3e
Kevin Lacker <lacker@gmail.com>
2018-08-09 11:14:31 -0700

    extension popup using react

--- js/src/main.js
+++ js/src/main.js
@@ -1,3 +1,8 @@
-chrome.browserAction.onClicked.addListener((tab) => {
-    alert("extension listener activated")
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.onload = () => {
+    const element = <h1>this is React</h1>;
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
