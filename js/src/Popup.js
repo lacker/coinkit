4d914799f693f80844d23b541c8fa51be3999a21
Kevin Lacker <lacker@gmail.com>
2018-08-09 12:36:01 -0700

    button, no hmr

--- js/src/Popup.js
+++ js/src/Popup.js
@@ -3,7 +3,24 @@
 import React, { Component } from "react";
 
 export default class Popup extends Component {
-  render() {
-    return <h1>hello world</h1>;
+  constructor(props) {
+    super(props);
+
+    this.state = { n: 0 };
+
+    this.click = this.click.bind(this);
+  }
+
+  click(newN) {
+    this.setState({ n: newN });
+  }
+
+  render(props) {
+    return (
+      <div>
+        <button onClick={() => this.click(this.state.n + 1)} />
+        <h1>hello world: {this.state.n}</h1>
+      </div>
+    );
   }
 }
