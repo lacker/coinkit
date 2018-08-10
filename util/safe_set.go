87a3ff4c7e4b79a57549b11b6eecd1848d6f750f
Kevin Lacker <lacker@gmail.com>
2018-08-10 11:42:44 -0700

    Safe set

---
+++ util/safe_set.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"sync"
+)
+
+// A SafeSet is threadsafe.
+type SafeSet struct {
+	mutex sync.Mutex
+	data  map[string]bool
+}
+
+func NewSafeSet() *SafeSet {
+	return &SafeSet{
+		data: make(map[string]bool),
+	}
+}
+
+func (set *SafeSet) Add(s string) {
+	set.mutex.Lock()
+	defer set.mutex.Unlock()
+
+	set.data[s] = true
+}
+
+func (set *SafeSet) Remove(s string) {
+	set.mutex.Lock()
+	defer set.mutex.Unlock()
+
+	delete(set.data, s)
+}
+
+func (set *SafeSet) Contains(s string) {
+	set.mutex.Lock()
+	defer set.mutex.Unlock()
+
+	_, ok := set.data[s]
+	return ok
+}
