5e19c3b52225cbfa0389128661ac2610c108a98e
Kevin Lacker <lacker@gmail.com>
2018-08-14 11:44:04 -0700

    keypair construction in js

--- js/src/KeyPair.js
+++ js/src/KeyPair.js
@@ -2,20 +2,30 @@
 // Annoyingly, our crypto library calls it a "secret key" while the Go library calls it
 // a "private key". We try to name things "private key" when possible here.
 
+import { toByteArray } from "base64-js";
 import nacl from "tweetnacl";
 
+// Adds padding to a base64-encoded string, which our library requires but some do not
+function base64pad(s) {
+  while (s.length % 4 != 0) {
+    s += "=";
+  }
+  return s;
+}
+
 export default class KeyPair {
-  // TODO: replace placeholder
-  constructor({ publicKey, privateKey }) {
+  constructor(publicKey, privateKey) {
     this.publicKey = publicKey;
     this.privateKey = privateKey;
+
+    if (publicKey.length != 32 || privateKey.length != 64) {
+      throw new Error("bad keys");
+    }
   }
 
   static fromPrivateKey(priv) {
-    let keys = nacl.sign.keyPair.fromSecretKey(priv);
-    return new KeyPair({
-      publicKey: keys.publicKey,
-      privateKey: keys.secretKey
-    });
+    let bytes = toByteArray(base64pad(priv));
+    let keys = nacl.sign.keyPair.fromSecretKey(bytes);
+    return new KeyPair(keys.publicKey, keys.secretKey);
   }
 }
