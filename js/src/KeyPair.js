03ce4c8c77e12a2846bd26232fe39d2ccabddd0a
Kevin Lacker <lacker@gmail.com>
2018-08-14 11:13:08 -0700

    at least static function works

--- js/src/KeyPair.js
+++ js/src/KeyPair.js
@@ -1,9 +1,21 @@
 // An ed25519 keypair. Designed to be parallel to the Go implementation.
+// Annoyingly, our crypto library calls it a "secret key" while the Go library calls it
+// a "private key". We try to name things "private key" when possible here.
+
+import nacl from "tweetnacl";
 
 export default class KeyPair {
   // TODO: replace placeholder
-  constructor() {
-    this.publicKey = "foo";
-    this.privateKey = "bar";
+  constructor({ publicKey, privateKey }) {
+    this.publicKey = publicKey;
+    this.privateKey = privateKey;
+  }
+
+  static fromPrivateKey(priv) {
+    let keys = nacl.sign.keyPair.fromSecretKey(priv);
+    return new KeyPair({
+      publicKey: keys.publicKey,
+      privateKey: keys.secretKey
+    });
   }
 }
--- js/src/KeyPair.test.js
+++ js/src/KeyPair.test.js
@@ -1,6 +1,9 @@
 import KeyPair from "./KeyPair";
 
-test("KeyPair has a public key", () => {
-  let kp = new KeyPair();
+test("KeyPair can be constructed from a private key", () => {
+  let kp = KeyPair.fromPrivateKey(
+    "1YBC5qpaM14DrVdsap5DtBWRv9IHf3Leyd95MOSSBV1cua0Uhxl/Y6afXFHIvFP+/m9V99AeVQndCtBV1E7/Tw"
+  );
   expect(kp.publicKey).toBeDefined();
+  expect(kp.privateKey).toBeDefined();
 });
