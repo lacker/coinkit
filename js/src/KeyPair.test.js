ded1368fc47e37b0ecdbced8fb3e96d04e5a7c11
Kevin Lacker <lacker@gmail.com>
2018-08-17 08:53:34 -0700

    oops redundant

--- js/src/KeyPair.test.js
+++ js/src/KeyPair.test.js
@@ -26,24 +26,6 @@ test("KeyPair crypto basics", () => {
   expect(sum.charCodeAt(0)).toBe(254);
 });
 
-test("KeyPair handling invalid public keys", () => {
-  expect(() => KeyPair.readPublicKey("blah")).toThrow();
-  expect(() => KeyPair.readPublicKey("0xblahblahblah")).toThrow();
-  expect(() =>
-    KeyPair.readPublicKey(
-      "0x12345678901234567890123456789012345678901234567890123456789012345678"
-    )
-  ).toThrow();
-});
-
-test("KeyPair can be constructed from a private key", () => {
-  let kp = KeyPair.fromPrivateKey(
-    "1YBC5qpaM14DrVdsap5DtBWRv9IHf3Leyd95MOSSBV1cua0Uhxl/Y6afXFHIvFP+/m9V99AeVQndCtBV1E7/Tw"
-  );
-  expect(kp.publicKey).toBeDefined();
-  expect(kp.privateKey).toBeDefined();
-});
-
 test("KeyPair.readPublicKey", () => {
   expect(() => {
     KeyPair.readPublicKey("blah");
@@ -60,6 +42,14 @@ test("KeyPair.readPublicKey", () => {
   KeyPair.readPublicKey(
     "0x5cb9ad1487197f63a69f5c51c8bc53fefe6f55f7d01e5509dd0ad055d44eff4f9a86"
   );
+});
+
+test("KeyPair can be constructed from a private key", () => {
+  let kp = KeyPair.fromPrivateKey(
+    "1YBC5qpaM14DrVdsap5DtBWRv9IHf3Leyd95MOSSBV1cua0Uhxl/Y6afXFHIvFP+/m9V99AeVQndCtBV1E7/Tw"
+  );
+  expect(kp.publicKey).toBeDefined();
+  expect(kp.privateKey).toBeDefined();
 });
 
 test("KeyPair specific signatures", () => {
