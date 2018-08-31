02779e066332149f1185ccba36734f3d2b4c559f
Kevin Lacker <lacker@gmail.com>
2018-08-31 08:45:44 -0700

    app loader

--- js/src/extension.js
+++ js/src/extension.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 
 import Popup from "./Popup";
 
+// This code runs to load the popup of the chrome extension.
+
 window.onload = () => {
   ReactDOM.render(<Popup />, document.getElementById("root"));
 };
