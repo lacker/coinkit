13805e03aa0fed5417ec70c3606ccb5f275445c3
Kevin Lacker <lacker@gmail.com>
2018-09-01 08:43:31 -0700

    sample app root component

---
+++ js/src/App.js
@@ -0,0 +1,17 @@
+// The root to display in the sample app.
+
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(props) {
+    return (
+      <div>
+        <h1>this is the sample app</h1>
+      </div>
+    );
+  }
+}
