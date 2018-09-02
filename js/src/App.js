53f6786824bff1a3f66547c86a8ddc9ca149fe9e
Kevin Lacker <lacker@gmail.com>
2018-09-02 14:01:38 -0700

    store user in state

--- js/src/App.js
+++ js/src/App.js
@@ -5,12 +5,17 @@ import React, { Component } from "react";
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      user: null
+    };
   }
 
   render(props) {
     return (
       <div>
         <h1>this is the sample app</h1>
+        <h1>{this.state.user || "nobody"} is logged in</h1>
       </div>
     );
   }
