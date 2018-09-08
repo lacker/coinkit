3ebe6a5d167dd27c2342ae8ff1f2bcedcfaa11d4
Kevin Lacker <lacker@gmail.com>
2018-09-08 08:51:10 -0700

    callback

--- js/src/Login.js
+++ js/src/Login.js
@@ -5,8 +5,11 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
 export default class Login extends Component {
+  // props.callback takes a keypair once the user has logged in
   constructor(props) {
     super(props);
+
+    this.callback = props.callback;
   }
 
   render() {
