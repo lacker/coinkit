b63beac86a973b62625f8e8d340b824c38383110
Kevin Lacker <lacker@gmail.com>
2018-09-09 17:34:21 -0700

    setPrivateKey

--- js/src/Login.js
+++ js/src/Login.js
@@ -4,12 +4,27 @@ import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
+import KeyPair from "./KeyPair";
+
 export default class Login extends Component {
   // props.callback takes a keypair once the user has logged in
   constructor(props) {
     super(props);
 
     this.callback = props.callback;
+  }
+
+  // Returns whether the private key is valid.
+  // If it is valid, calls the callback on the associated keypair.
+  setPrivateKey(privateKey) {
+    let kp = null;
+    try {
+      kp = KeyPair.fromPrivateKey(privateKey);
+    } catch (e) {
+      return false;
+    }
+    this.callback(kp);
+    return true;
   }
 
   render() {
