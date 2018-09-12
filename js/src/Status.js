10165bc1bd4ffc73b5b42ae40a0750f47ffa0e65
Kevin Lacker <lacker@gmail.com>
2018-09-12 09:46:05 -0700

    Status component

---
+++ js/src/Status.js
@@ -0,0 +1,42 @@
+// A screen to show the status of your account.
+
+import React, { Component } from "react";
+
+export default class Status extends Component {
+  // props.popup is a reference to the root popup
+  // props.keyPair is the key pair
+  // props.balance is the account balance
+  constructor(props) {
+    super(props);
+
+    this.popup = props.popup;
+    this.keyPair = props.keyPair;
+    this.balance = props.balance;
+    this.logOut = this.logOut.bind(this);
+  }
+
+  logOut() {
+    this.popup.newKeyPair(null);
+  }
+
+  render() {
+    let style = {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "flex-start",
+      alignItems: "center",
+      width: 360,
+      padding: 30
+    };
+    return (
+      <div style={style}>
+        <h1>Status</h1>
+        <div>Public key: {this.keyPair.publicKey}</div>
+        <div>Balance: {this.balance || "..."}</div>
+        <div style={{ color: "blue", cursor: "pointer" }} onClick={this.logOut}>
+          Log out
+        </div>
+      </div>
+    );
+  }
+}
