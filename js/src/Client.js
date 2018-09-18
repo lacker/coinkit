df7d94a7e3725089b5caaa565ff592111b606741
Kevin Lacker <lacker@gmail.com>
2018-09-18 10:39:59 -0700

    update client and content script

--- js/src/Client.js
+++ js/src/Client.js
@@ -1,3 +1,5 @@
+import Message from "./Message";
+
 // Client is designed to be included in applications and run in untrusted application
 // environment. It gets permissions by requesting them from the extension, whose code
 // is trusted.
@@ -5,6 +7,41 @@ export default class Client {
   constructor() {
     // publicKey is null before permissions are acquired
     this.publicKey = null;
+
+    // Callbacks are keyed by message id
+    this.callbacks = {};
+
+    window.addEventListener("message", event => {
+      if (event.source != window || event.data.type != "fromCoinkit") {
+        return;
+      }
+
+      let callback = this.callbacks[event.data.id];
+      delete this.callbacks[event.data.id];
+      if (!callback) {
+        return;
+      }
+
+      callback(Message.fromSerialized(event.data.message));
+    });
+  }
+
+  getMessageId() {
+    return "" + Math.random();
+  }
+
+  async sendMessage(message) {
+    let id = this.getMessageId();
+    this.nextId++;
+    let data = {
+      id: id,
+      type: "toCoinkit",
+      message: message.serialize()
+    };
+    return new Promise((resolve, reject) => {
+      this.callbacks[id] = resolve;
+      window.postMessage(data);
+    });
   }
 
   // Requests public key permission from the extension if we don't already have it.
--- js/src/content-main.js
+++ js/src/content-main.js
@@ -1,3 +1,15 @@
 // This code runs in our content script, in the context of every web page.
 
-console.log("XXX this is content-main");
+console.log("XXX this is content main take 3");
+
+window.addEventListener(
+  "message",
+  event => {
+    if (event.source != window || event.data.type != "toCoinkit") {
+      return;
+    }
+
+    console.log("XXX content script received", event.data);
+  },
+  false
+);
