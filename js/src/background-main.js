09135bbe27901d50afa55b93e991a91a9eb7c481
Kevin Lacker <lacker@gmail.com>
2018-09-25 13:51:59 -0700

    react redux

--- js/src/background-main.js
+++ js/src/background-main.js
@@ -1,6 +1,12 @@
 // This code runs in the persistent background page.
+
+import { createStore } from "redux";
+
 import Storage from "./Storage";
 import TrustedClient from "./TrustedClient";
 
+import reducers from "./reducers";
+
 window.client = new TrustedClient();
 window.storage = new Storage();
+window.store = createStore(reducers);
