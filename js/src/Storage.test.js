a629973b3b582b7ee50303b7e645606234a25fdf
Kevin Lacker <lacker@gmail.com>
2018-10-14 14:55:33 -0700

    make a Storage

--- js/src/Storage.test.js
+++ js/src/Storage.test.js
@@ -1,3 +1,6 @@
+import MockLocalStorage from "./MockLocalStorage";
 import Storage from "./Storage";
 
-test("basic redux flow", () => {});
+test("basic redux flow", () => {
+  let storage = new Storage(MockLocalStorage());
+});
