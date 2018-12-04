f6a8ed173957072c61028e2e422ab8aa2b757678
Kevin Lacker <lacker@gmail.com>
2018-12-04 14:20:34 -0800

    start a libp2p server

--- hosting/server.go
+++ hosting/server.go
@@ -1,11 +1,32 @@
 package hosting
 
-import ()
+import (
+	"context"
+	"fmt"
+
+	libp2p "github.com/libp2p/go-libp2p"
+)
 
 type Server struct {
-	port int
+	context context.Context
+	cancel  context.CancelFunc
 }
 
-func NewServer(port int) *Server {
-	return &Server{port: port}
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) Serve() {
+	s.context, s.cancel = context.WithCancel(context.Background())
+
+	host, err := libp2p.New(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("hello world. host id is %s\n", host.ID())
+}
+
+func (s *Server) Quit() {
+	s.cancel()
 }
