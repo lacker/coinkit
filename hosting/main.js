cb968e198bc9744a7386fe2d0d1d4c1d8ce25928
Kevin Lacker <lacker@gmail.com>
2018-12-05 10:51:19 -0800

    seed logic

--- hosting/main.js
+++ hosting/main.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+const WebTorrent = require("webtorrent-hybrid");
+
+let buf = new Buffer("Hello decentralized world!");
+buf.name = "hello.txt";
+let client = new WebTorrent();
+client.seed(buf, () => {
+  console.log("seeding has begun");
+});
