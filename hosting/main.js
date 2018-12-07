43ec82fe9b219458a4a5510e41bd97053324196e
Kevin Lacker <lacker@gmail.com>
2018-12-07 10:22:35 -0800

    comments

--- hosting/main.js
+++ hosting/main.js
@@ -1,5 +1,12 @@
 const WebTorrent = require("webtorrent-hybrid");
 
+// Run a black hole proxy - this just returns a blank document for every URL.
+// This is needed so that the extension can pretend a URL that doesn't resolve is
+// actually a functioning web page.
+
+// TODO: see if we can just muck with the error page
+
+// Seed a WebTorrent
 let buf = new Buffer("Hello decentralized world!");
 buf.name = "hello.txt";
 let client = new WebTorrent();
