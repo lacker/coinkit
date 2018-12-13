8498acf9fcb3ef169fb783b3dee625004b00530a
Kevin Lacker <lacker@gmail.com>
2018-12-13 12:26:18 -0800

    todo

--- js/src/TorrentClient.js
+++ js/src/TorrentClient.js
@@ -1,5 +1,7 @@
 // The TorrentClient finds the right torrent for a domain and uses that to return subsequent files.
 
+// TODO: give this functions that lets it serve up subsequent files like image files
+
 import WebTorrent from "webtorrent";
 
 class TorrentClient {
