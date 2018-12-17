e24952cf07f22770ae1ca6d23e1ce5255fbaaba5
Kevin Lacker <lacker@gmail.com>
2018-12-17 08:17:04 -0800

    loadDomain

--- js/src/TorrentClient.js
+++ js/src/TorrentClient.js
@@ -10,10 +10,13 @@ let BOOTSTRAP = "http://localhost:4444";
 class TorrentClient {
   constructor() {
     this.client = new WebTorrent();
+
+    // Maps domain to torrent
+    this.torrents = {};
   }
 
   // Starts downloading and resolves to a torrent object when the download finishes
-  async download(magnet) {
+  async loadMagnet(magnet) {
     return new Promise((resolve, reject) => {
       this.client.add(magnet, torrent => {
         torrent.on("done", () => {
@@ -30,9 +33,11 @@ class TorrentClient {
     return json.magnet;
   }
 
-  // TODO: describe what and when this returns
-  async startDownloading(domain) {
-    let magnet = this.getMagnet(domain);
-    // TODO: more
+  // Starts downloading a domain and resolves when the root URL is ready
+  async loadDomain(domain) {
+    let magnet = await this.getMagnet(domain);
+    let torrent = await this.loadMagnet(magnet);
+    this.torrents[domain] = torrent;
+    return;
   }
 }
