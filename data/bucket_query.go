e0e75c5bce9dce6c6b471e724a201910737cd807
Kevin Lacker <lacker@gmail.com>
2019-01-15 09:46:57 -0800

    clean up autogend sql

--- data/bucket_query.go
+++ data/bucket_query.go
@@ -6,18 +6,19 @@ import (
 )
 
 type BucketQuery struct {
-	Name  string `json:"name"`
-	Owner string `json:"owner"`
-	Limit int    `json:"limit"`
+	Name     string `json:"name"`
+	Owner    string `json:"owner"`
+	Limit    int    `json:"limit"`
+	Provider uint64 `json:"provider"`
 }
 
 func (q *BucketQuery) String() string {
 	parts := []string{}
 	if q.Name != "" {
-		parts = append(parts, fmt.Sprintf("name=%s", q.Name))
+		parts = append(parts, fmt.Sprintf("name = %s", q.Name))
 	}
 	if q.Owner != "" {
-		parts = append(parts, fmt.Sprintf("owner=%s", q.Owner))
+		parts = append(parts, fmt.Sprintf("owner = %s", q.Owner))
 	}
 	if len(parts) == 0 {
 		return "<empty>"
--- data/provider_query.go
+++ data/provider_query.go
@@ -14,10 +14,10 @@ type ProviderQuery struct {
 func (q *ProviderQuery) String() string {
 	parts := []string{}
 	if q.ID != 0 {
-		parts = append(parts, fmt.Sprintf("id=%d", q.ID))
+		parts = append(parts, fmt.Sprintf("id = %d", q.ID))
 	}
 	if q.Owner != "" {
-		parts = append(parts, fmt.Sprintf("owner=%s", q.Owner))
+		parts = append(parts, fmt.Sprintf("owner = %s", q.Owner))
 	}
 	if len(parts) == 0 {
 		return "<empty>"
