a56a188b65b21588cc088c081ce7f2d90b8dce70
Kevin Lacker <lacker@gmail.com>
2019-01-17 10:22:25 -0800

    dpo

---
+++ data/delete_provider_operation.go
@@ -0,0 +1,61 @@
+package data
+
+import (
+	"fmt"
+
+	"github.com/lacker/coinkit/util"
+)
+
+type DeleteProviderOperation struct {
+	// Who is deleting this provider. Must be the owner
+	Signer string `json:"signer"`
+
+	// The sequence number for this operation
+	Sequence uint32 `json:"sequence"`
+
+	// The operation fee for entering an op into the blockchain
+	Fee uint64 `json:"fee"`
+
+	// The id of the provider to delete
+	ID uint64 `json:"id"`
+}
+
+func (op *DeleteProviderOperation) String() string {
+	return fmt.Sprintf("DeleteProvider owner=%s, id=%d",
+		util.Shorten(op.Signer), op.ID)
+}
+
+func (op *DeleteProviderOperation) OperationType() string {
+	return "DeleteProvider"
+}
+
+func (op *DeleteProviderOperation) GetSigner() string {
+	return op.Signer
+}
+
+func (op *DeleteProviderOperation) GetFee() uint64 {
+	return op.Fee
+}
+
+func (op *DeleteProviderOperation) GetSequence() uint32 {
+	return op.Sequence
+}
+
+// TODO: should this do something?
+func (op *DeleteProviderOperation) Verify() bool {
+	return true
+}
+
+func MakeTestDeleteProviderOperation(id uint64, n int) *SignedOperation {
+	mint := util.NewKeyPairFromSecretPhrase("mint")
+	op := &DeleteProviderOperation{
+		Signer:   mint.PublicKey().String(),
+		Sequence: uint32(n),
+		ID:       it,
+	}
+	return NewSignedOperation(op, mint)
+}
+
+func init() {
+	RegisterOperationType(&DeleteProviderOperation{})
+}
