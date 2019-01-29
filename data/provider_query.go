ee7357087615d0009733c0bd876f0811c666e10c
Kevin Lacker <lacker@gmail.com>
2019-01-29 10:26:46 -0800

    IDs in providerquery

--- data/provider_query.go
+++ data/provider_query.go
@@ -6,15 +6,19 @@ import (
 )
 
 type ProviderQuery struct {
-	ID    uint64 `json:"id"`
-	Owner string `json:"owner"`
-	Limit int    `json:"limit"`
+	ID    uint64   `json:"id"`
+	IDs   []uint64 `json:"ids"`
+	Owner string   `json:"owner"`
+	Limit int      `json:"limit"`
 }
 
 func (q *ProviderQuery) String() string {
 	parts := []string{}
 	if q.ID != 0 {
 		parts = append(parts, fmt.Sprintf("id=%d", q.ID))
+	}
+	if len(q.IDs) != 0 {
+		parts = append(parts, fmt.Sprintf("ids=#+v", q.IDs))
 	}
 	if q.Owner != "" {
 		parts = append(parts, fmt.Sprintf("owner=%s", q.Owner))
