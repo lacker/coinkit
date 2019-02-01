7f40bd247b055587d1511ce6a6ef6576bf55f987
Kevin Lacker <lacker@gmail.com>
2019-02-01 14:13:12 -0800

    dealloc op

--- data/deallocate_operation.go
+++ data/deallocate_operation.go
@@ -6,8 +6,8 @@ import (
 	"github.com/lacker/coinkit/util"
 )
 
-type AllocateOperation struct {
-	// Who is performing this allocation. Can be either the bucket or provider owner
+type DeallocateOperation struct {
+	// Who is performing this deallocation. Can be either the bucket or provider owner
 	Signer string `json:"signer"`
 
 	// The sequence number for this operation
@@ -23,28 +23,28 @@ type AllocateOperation struct {
 	ID uint64 `json:"id"`
 }
 
-func (op *AllocateOperation) String() string {
-	return fmt.Sprintf("Allocate signer=%s, name=%s, id=%d",
+func (op *DeallocateOperation) String() string {
+	return fmt.Sprintf("Deallocate signer=%s, name=%s, id=%d",
 		util.Shorten(op.Signer), op.Name, op.ID)
 }
 
-func (op *AllocateOperation) OperationType() string {
-	return "Allocate"
+func (op *DeallocateOperation) OperationType() string {
+	return "Deallocate"
 }
 
-func (op *AllocateOperation) GetSigner() string {
+func (op *DeallocateOperation) GetSigner() string {
 	return op.Signer
 }
 
-func (op *AllocateOperation) GetFee() uint64 {
+func (op *DeallocateOperation) GetFee() uint64 {
 	return op.Fee
 }
 
-func (op *AllocateOperation) GetSequence() uint32 {
+func (op *DeallocateOperation) GetSequence() uint32 {
 	return op.Sequence
 }
 
-func (op *AllocateOperation) Verify() bool {
+func (op *DeallocateOperation) Verify() bool {
 	if !IsValidBucketName(op.Name) {
 		return false
 	}
@@ -55,5 +55,5 @@ func (op *AllocateOperation) Verify() bool {
 }
 
 func init() {
-	RegisterOperationType(&AllocateOperation{})
+	RegisterOperationType(&DeallocateOperation{})
 }
