2dd05fbed9790dcb2434fab565c99e6ec9b7e71b
Kevin Lacker <lacker@gmail.com>
2019-02-12 09:35:47 -0800

    ChainClient

---
+++ hosting/ChainClient.js
@@ -0,0 +1,12 @@
+// A Node client for talking to the blockchain servers
+// TODO: think about how to combine some of this code with the web js client
+
+// TODO: load this in some way that distinguishes between local testing, and prod
+let URLS = [
+  "http://localhost:8000",
+  "http://localhost:8001",
+  "http://localhost:8002",
+  "http://localhost:8003"
+];
+
+export default class ChainClient {}
