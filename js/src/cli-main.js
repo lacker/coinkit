9325d96bc60e19c613fb578af45cd4495ec6e49e
Kevin Lacker <lacker@gmail.com>
2019-02-13 10:35:02 -0800

    handle bad cli args

--- js/src/cli-main.js
+++ js/src/cli-main.js
@@ -1,5 +1,10 @@
 let args = process.argv.slice(2);
 
+if (args.length == 0) {
+  console.log("No command line arguments found");
+  process.exit(1);
+}
+
 args.forEach(val => {
   console.log("CLI arg:", val);
 });
