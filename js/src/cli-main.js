bd14d2ac89583c63603de07549b522f05341a1ad
Kevin Lacker <lacker@gmail.com>
2019-02-14 09:54:03 -0800

    ask fn

--- js/src/cli-main.js
+++ js/src/cli-main.js
@@ -1,10 +1,35 @@
+let readline = require("readline");
+
 function fatal(message) {
   console.log(message);
   process.exit(1);
 }
 
+// Asks the CLI user a question, asynchronously returns the response
+async function ask(question) {
+  let r = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  let p = new Promise((resolve, reject) => {
+    r.question(question, answer => {
+      r.close();
+      resolve(answer);
+    });
+  });
+
+  return await p;
+}
+
 function generate() {
   fatal("TODO: implement generate");
+}
+
+// Ask the user for a passphrase to log in.
+// Returns the keypair
+async function login() {
+  console.log("please enter your passphrase:");
 }
 
 function main() {
