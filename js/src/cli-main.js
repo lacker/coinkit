342d6d140e06c33b1e2d25d632ccdf2accf4c402
Kevin Lacker <lacker@gmail.com>
2019-02-14 09:27:54 -0800

    main fn

--- js/src/cli-main.js
+++ js/src/cli-main.js
@@ -1,25 +1,29 @@
-let args = process.argv.slice(2);
-
 function fatal(message) {
   console.log(message);
   process.exit(1);
 }
 
-if (args.length == 0) {
-  fatal("usage: npm run cli <operation> <arguments>");
-}
-
-let op = args[0];
-let rest = args.slice(1);
-
-if (op === "generate") {
-  if (rest.length != 0) {
-    fatal("usage: npm run cli generate");
-  }
-
-  generate();
-}
-
 function generate() {
   fatal("TODO: implement generate");
 }
+
+function main() {
+  let args = process.argv.slice(2);
+
+  if (args.length == 0) {
+    fatal("usage: npm run cli <operation> <arguments>");
+  }
+
+  let op = args[0];
+  let rest = args.slice(1);
+
+  if (op === "generate") {
+    if (rest.length != 0) {
+      fatal("usage: npm run cli generate");
+    }
+
+    generate();
+  }
+}
+
+main();
