21e3701eac66ea7ab2a0c212aca0b0afe5f5e7c8
Kevin Lacker <lacker@gmail.com>
2019-03-08 09:00:20 -0800

    fix go test

--- data/operation_message_test.go
+++ data/operation_message_test.go
@@ -42,7 +42,7 @@ func TestOperationMessages(t *testing.T) {
 
 // Also see tests of this string in TrustedClient.test.js
 func TestCreateDocumentOperationMessageFromJS(t *testing.T) {
-	serialized := "e:0x5b8f312caed13ac35805c69e889d24bbd3df7d6285fbca173cce47e7402a5d0bddf3:a09g9sLYa7xUSdLvG1K1r4kaD9Iu2+bMjFhQtnWUKrvK/UPHh3GpcZPNLbDc1vibkZs1TqF1QNz9B2u7FEzjBA:{\"message\":{\"operations\":[{\"operation\":{\"data\":{\"foo\":\"bar\"},\"fee\":1,\"sequence\":1,\"signer\":\"0x5b8f312caed13ac35805c69e889d24bbd3df7d6285fbca173cce47e7402a5d0bddf3\"},\"signature\":\"a6a+P+y0UkhFLXonoQXobIMFCXJFGyMiCc5yjQDOa0fz9jnaQf/LKYkzN2CII4nSQjIiromm/bzOaLJumQfpCg\",\"type\":\"Create\"}]},\"type\":\"Operation\"}"
+	serialized := "e:0x5b8f312caed13ac35805c69e889d24bbd3df7d6285fbca173cce47e7402a5d0bddf3:/zpIpa4ZZ/1AVAvP7mnwlr1D+XAfYX+UNeFx+UvIlv0UTYUFXnRuTveao4ULm/O8tWrOzKLHP8BgAJEN05JUCg:{\"message\":{\"operations\":[{\"operation\":{\"data\":{\"foo\":\"bar\"},\"fee\":1,\"sequence\":1,\"signer\":\"0x5b8f312caed13ac35805c69e889d24bbd3df7d6285fbca173cce47e7402a5d0bddf3\"},\"signature\":\"powQVmQmIPLMs8InVatDw0MY3Olc4G3P8p6CE/ikgVElad6cXW0jCpFC9pD0bIOAHZmXS80U9RPKUupZSA92BQ\",\"type\":\"CreateDocument\"}]},\"type\":\"Operation\"}"
 	msg, err := util.NewSignedMessageFromSerialized(serialized)
 	if err != nil {
 		t.Fatalf("could not decode signed message: %s", err)
