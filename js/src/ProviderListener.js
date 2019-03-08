965180c698d3826d99eaea3aff66a08548ae3d45
Kevin Lacker <lacker@gmail.com>
2019-03-08 09:15:35 -0800

    desc

--- js/src/ProviderListener.js
+++ js/src/ProviderListener.js
@@ -1,5 +1,7 @@
 const ChainClient = require("./ChainClient.js");
 
+// The ProviderListener continuously tracks information relevant to a single provider.
+// This is designed to be the source of information for a hosting server.
 class ProviderListener {
   constructor(id) {
     this.id = id;
