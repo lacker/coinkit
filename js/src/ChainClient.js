f8171cdc9c3731e338acc5cfd0785d16f04fd3d2
Kevin Lacker <lacker@gmail.com>
2019-03-12 09:57:53 -0700

    reorder, and new ub

--- js/src/ChainClient.js
+++ js/src/ChainClient.js
@@ -59,98 +59,42 @@ class ChainClient {
     }
   }
 
-  // Keeps re-fetching the provider until it exists.
-  async waitForProvider(providerID) {
-    this.log("waiting for provider", providerID, "to be created");
-    while (true) {
-      let provider = await this.getProvider(providerID);
-      if (provider !== null) {
-        return provider;
-      }
-      this.log("not yet, still waiting");
-      await standardWait();
-    }
-  }
+  // Sends a Message upstream, signing with our keypair.
+  // Returns a promise for the response Message.
+  // If the response is an error message, we throw an error with the provided error string.
+  async sendMessage(message) {
+    let clientMessage = SignedMessage.fromSigning(message, this.keyPair);
+    let url = getServerURL() + "/messages";
+    let body = clientMessage.serialize() + "\n";
+    this.log("sending body:", body);
+    let response = await axios.post(url, body, {
+      headers: { "Content-Type": "text/plain" },
+      responseType: "text"
+    });
+    let text = response.data;
+    let serialized = text.replace(/\n$/, "");
 
-  // Fetches the provider with the given provider id.
-  // If there is no such provider, returns null.
-  async getProvider(providerID) {
-    let dm = await this.query({ providers: { id: providerID } });
-    if (!dm.providers || dm.providers.length == 0) {
+    // When there is an empty keepalive message from the server, we just return null
+    let signed = SignedMessage.fromSerialized(serialized);
+    if (signed == null) {
       return null;
     }
-    return dm.providers[0];
+
+    if (signed.message.type === "Error") {
+      throw new Error(signed.message.error);
+    }
+    return signed.message;
   }
 
-  // Returns the information for the newly-created provider.
-  async createProvider(capacity) {
-    if (typeof capacity !== "number") {
-      throw new Error(
-        "capacity " + capacity + " must be number, not " + typeof capacity
-      );
+  // Sends a query message. Returns the data message response.
+  // Throws an error if we get an error message back.
+  async query(params) {
+    let qm = new Message("Query", params);
+    let dm = await this.sendMessage(qm);
+    if (dm.type === "Error") {
+      throw new Error(dm.error);
     }
-
-    // To figure out which provider is newly-created, we need to check existing ones
-    let user = this.keyPair.getPublicKey();
-    let initialProviders = await this.getProviders({ owner: user });
-    this.log("existing providers:", initialProviders);
-    await this.sendOperation("CreateProvider", { capacity });
-    this.log("the CreateProvider operation has been accepted");
-    let providers = await this.getProviders({ owner: user });
-    this.log("new providers:", providers);
-
-    for (let provider of providers) {
-      if (!initialProviders.find(p => p.id === provider.id)) {
-        // This provider wasn't in the initial set
-        return provider;
-      }
-    }
-
-    throw new Error("no provider seems to have been created");
-  }
-
-  // Returns the information for the newly-created bucket.
-  async createBucket(name, size) {
-    if (typeof size !== "number") {
-      throw new Error(
-        "bucket size " + size + " must be number, not " + typeof size
-      );
-    }
-
-    await this.sendOperation("CreateBucket", { name, size });
-    this.log("the CreateBucket operation has been accepted");
-    let bucket = await this.getBucket(name);
-    return bucket;
-  }
-
-  async allocate(bucketName, providerID) {
-    if (typeof providerID !== "number") {
-      throw new Error(
-        "provider id " +
-          providerID +
-          " must be number, not " +
-          typeof providerID
-      );
-    }
-
-    await this.sendOperation("Allocate", { bucketName, providerID });
-    this.log("the Allocate operation has been accepted");
-    return;
-  }
-
-  async deallocate(bucketName, providerID) {
-    if (typeof providerID !== "number") {
-      throw new Error(
-        "provider id " +
-          providerID +
-          " must be number, not " +
-          typeof providerID
-      );
-    }
-
-    await this.sendOperation("Deallocate", { bucketName, providerID });
-    this.log("the Deallocate operation has been accepted");
-    return;
+    return dm;
   }
 
   // Returns once the operation has been accepted into the blockchain.
@@ -195,6 +139,73 @@ class ChainClient {
     }
   }
 
+  // Fetches the account with the given user, or null if there is no such account.
+  async getAccount(user) {
+    let dm = await this.query({ account: user });
+    if (!dm.accounts || !dm.accounts[user]) {
+      return null;
+    }
+    return dm.accounts[user];
+  }
+
+  // Fetches data for providers according to the given query.
+  // "owner" and "bucket" keys are the most likely.
+  // Returns a list of providers in no particular order.
+  async getProviders(query) {
+    let dm = await this.query({ providers: query });
+    return dm.providers;
+  }
+
+  // Fetches the provider with the given provider id.
+  // If there is no such provider, returns null.
+  async getProvider(providerID) {
+    let dm = await this.query({ providers: { id: providerID } });
+    if (!dm.providers || dm.providers.length == 0) {
+      return null;
+    }
+    return dm.providers[0];
+  }
+
+  // Keeps re-fetching the provider until it exists.
+  async waitForProvider(providerID) {
+    this.log("waiting for provider", providerID, "to be created");
+    while (true) {
+      let provider = await this.getProvider(providerID);
+      if (provider !== null) {
+        return provider;
+      }
+      this.log("not yet, still waiting");
+      await standardWait();
+    }
+  }
+
+  // Returns the information for the newly-created provider.
+  async createProvider(capacity) {
+    if (typeof capacity !== "number") {
+      throw new Error(
+        "capacity " + capacity + " must be number, not " + typeof capacity
+      );
+    }
+
+    // To figure out which provider is newly-created, we need to check existing ones
+    let user = this.keyPair.getPublicKey();
+    let initialProviders = await this.getProviders({ owner: user });
+    this.log("existing providers:", initialProviders);
+    await this.sendOperation("CreateProvider", { capacity });
+    this.log("the CreateProvider operation has been accepted");
+    let providers = await this.getProviders({ owner: user });
+    this.log("new providers:", providers);
+
+    for (let provider of providers) {
+      if (!initialProviders.find(p => p.id === provider.id)) {
+        // This provider wasn't in the initial set
+        return provider;
+      }
+    }
+
+    throw new Error("no provider seems to have been created");
+  }
+
   // Fetches data for the listed buckets.
   // Returns a list of buckets in no particular order.
   async getBuckets(query) {
@@ -212,63 +223,55 @@ class ChainClient {
     return buckets[0];
   }
 
+  // Returns the information for the newly-created bucket.
+  async createBucket(name, size) {
+    if (typeof size !== "number") {
+      throw new Error(
+        "bucket size " + size + " must be number, not " + typeof size
+      );
+    }
+
+    await this.sendOperation("CreateBucket", { name, size });
+    this.log("the CreateBucket operation has been accepted");
+    let bucket = await this.getBucket(name);
+    return bucket;
+  }
+
   async updateBucket(name, magnet) {
-    // XXX
+    await this.sendOperation("UpdateBucket", { name, magnet });
+    this.log("the UpdateBucket operation has been accepted");
+    let bucket = await this.getBucket(name);
+    return bucket;
   }
 
-  // Fetches data for providers according to the given query.
-  // "owner" and "bucket" keys are the most likely.
-  // Returns a list of providers in no particular order.
-  async getProviders(query) {
-    let dm = await this.query({ providers: query });
-    return dm.providers;
+  async allocate(bucketName, providerID) {
+    if (typeof providerID !== "number") {
+      throw new Error(
+        "provider id " +
+          providerID +
+          " must be number, not " +
+          typeof providerID
+      );
+    }
+
+    await this.sendOperation("Allocate", { bucketName, providerID });
+    this.log("the Allocate operation has been accepted");
+    return;
   }
 
-  // Fetches the account with the given user, or null if there is no such account.
-  async getAccount(user) {
-    let dm = await this.query({ account: user });
-    if (!dm.accounts || !dm.accounts[user]) {
-      return null;
-    }
-    return dm.accounts[user];
-  }
-
-  // Sends a query message. Returns the data message response.
-  // Throws an error if we get an error message back.
-  async query(params) {
-    let qm = new Message("Query", params);
-    let dm = await this.sendMessage(qm);
-    if (dm.type === "Error") {
-      throw new Error(dm.error);
-    }
-    return dm;
-  }
-
-  // Sends a Message upstream, signing with our keypair.
-  // Returns a promise for the response Message.
-  // If the response is an error message, we throw an error with the provided error string.
-  async sendMessage(message) {
-    let clientMessage = SignedMessage.fromSigning(message, this.keyPair);
-    let url = getServerURL() + "/messages";
-    let body = clientMessage.serialize() + "\n";
-    this.log("sending body:", body);
-    let response = await axios.post(url, body, {
-      headers: { "Content-Type": "text/plain" },
-      responseType: "text"
-    });
-    let text = response.data;
-    let serialized = text.replace(/\n$/, "");
-
-    // When there is an empty keepalive message from the server, we just return null
-    let signed = SignedMessage.fromSerialized(serialized);
-    if (signed == null) {
-      return null;
+  async deallocate(bucketName, providerID) {
+    if (typeof providerID !== "number") {
+      throw new Error(
+        "provider id " +
+          providerID +
+          " must be number, not " +
+          typeof providerID
+      );
     }
 
-    if (signed.message.type === "Error") {
-      throw new Error(signed.message.error);
-    }
-    return signed.message;
+    await this.sendOperation("Deallocate", { bucketName, providerID });
+    this.log("the Deallocate operation has been accepted");
+    return;
   }
 }
 
