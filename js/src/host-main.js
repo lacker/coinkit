282f4a863687a62097b9cbe91081700de8f32a11
Kevin Lacker <lacker@gmail.com>
2019-03-12 09:48:55 -0700

    host cmts

--- js/src/host-main.js
+++ js/src/host-main.js
@@ -1,6 +1,6 @@
-// This is the entry point for the Node hosting server that miners run to store files.
+// This is the entry point for the node hosting server that miners run to store files.
 //
-// You may or may not first have to run:
+// TODO: see if this is still needed. You may or may not first have to run:
 // `sudo apt-get install xvfb`
 // `sudo apt-get install libgconf-2-4`
 // It used to be necessary but the webtorrent library may have gotten updated.
