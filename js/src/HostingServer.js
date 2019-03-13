d4c4a255a8d395e4bfc121d58362d4a2c786cc4f
Kevin Lacker <lacker@gmail.com>
2019-03-13 10:24:39 -0700

    HostingServer.server

--- js/src/HostingServer.js
+++ js/src/HostingServer.js
@@ -5,11 +5,17 @@
 // `sudo apt-get install libgconf-2-4`
 // It used to be necessary but the webtorrent library may have gotten updated.
 
+const ProviderListener = require("./ProviderListener.js");
+
 class HostingServer {
   // id is the provider id we are hosting for
   constructor(id) {
     this.id = id;
     this.verbose = false;
+
+    this.listener = new ProviderListener();
+    this.listener.onAdd(magnet => this.add(magnet));
+    this.listener.onRemove(magnet => this.remove(magnet));
   }
 
   log(...args) {
@@ -24,6 +30,10 @@ class HostingServer {
 
   remove(magnet) {
     this.log("removing magnet:", magnet);
+  }
+
+  async serve() {
+    await this.listener.listen(this.id);
   }
 }
 
