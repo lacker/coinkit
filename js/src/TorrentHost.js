7e3cb5ea05563f237a0decf1dfa28008403c4bcc
Kevin Lacker <lacker@gmail.com>
2019-03-14 09:03:42 -0700

    th shell

---
+++ js/src/TorrentHost.js
@@ -0,0 +1,10 @@
+// The TorrentHost hosts a webtorrent, possibly over a long period of time, running
+// on node, backed by the filesystem.
+
+const WebTorrent = require("webtorrent");
+
+class TorrentHost {
+  constructor() {}
+}
+
+module.exports = TorrentHost;
