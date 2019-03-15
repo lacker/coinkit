470eb7aa787a58357bb103cf89fedb89323a63e9
Kevin Lacker <lacker@gmail.com>
2019-03-15 09:35:46 -0700

    test that catches break go-side

--- data/operation_message_test.go
+++ data/operation_message_test.go
@@ -57,3 +57,11 @@ func TestCreateDocumentOperationMessageFromJS(t *testing.T) {
 		t.Fatalf("expected one operation but got %v", opm.Operations)
 	}
 }
+
+func TestUpdateBucketOperationMessageFromJS(t *testing.T) {
+	serialized := `e:0x32652ebe42a8d56314b8b11abf51c01916a238920c1f16db597ee87374515f4609d3:9n+eaCZIoJr8TOWS3LJNkK7GbzIRugfNGtn/tTWwPD2IzQYX6667sVESxH+lLkIopz9iMJUZdG/B3BoT3w7hCQ:{"message":{"operations":[{"operation":{"fee":0,"magnet":"magnet:?xt=urn:btih:d946a7e88aeafdf6516d7d93fba9d786f303a593&dn=samplesite&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com","name":"hello","sequence":3,"signer":"0x32652ebe42a8d56314b8b11abf51c01916a238920c1f16db597ee87374515f4609d3"},"signature":"UhMtJ+b7u6hA59uK/yZEX3XGpiQ8khQADlchFGu93QN09IbqqMBjzFQnQYGTZhT1zzbrgDm3rf0gbqCedyXLAQ","type":"UpdateBucket"}]},"type":"Operation"}`
+	_, err := util.NewSignedMessageFromSerialized(serialized)
+	if err != nil {
+		t.Fatalf("could not decode signed message: %s", err)
+	}
+}
