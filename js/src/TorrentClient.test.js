8884e02e0c3bcfb97bd5ab51b88927160de5fc83
Kevin Lacker <lacker@gmail.com>
2019-03-25 14:46:58 -0700

    test downloading

--- js/src/TorrentClient.test.js
+++ js/src/TorrentClient.test.js
@@ -3,6 +3,16 @@ const path = require("path");
 const TorrentClient = require("./TorrentClient.js");
 
 test("Seeding and downloading", async () => {
-  let client = new TorrentClient();
+  // Seed a torrent
+  let seedClient = new TorrentClient();
   let dir = path.resolve(__dirname, "samplesite");
+  let t = await seedClient.seed(dir);
+  let magnet = t.magnet;
+
+  // Download that exact torrent
+  let downloadClient = new TorrentClient();
+  let torrent = await downloadClient.download(magnet);
+  await torrent.waitForDone();
+
+  // TODO: check that the download works
 });
