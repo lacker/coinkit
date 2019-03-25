60dcc3f05a46a330538a6326ecbb811a697e6391
Kevin Lacker <lacker@gmail.com>
2019-03-25 14:44:37 -0700

    samplesite in test

--- js/src/TorrentClient.test.js
+++ js/src/TorrentClient.test.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 const TorrentClient = require("./TorrentClient.js");
 
 test("Seeding and downloading", async () => {
   let client = new TorrentClient();
+  let dir = path.resolve(__dirname, "samplesite");
 });
