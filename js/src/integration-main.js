0a2201cef257585d9b57b3bc4097939ae3f512db
Kevin Lacker <lacker@gmail.com>
2019-03-26 14:23:56 -0700

    integration test successfully downloads *something*

--- js/src/integration-main.js
+++ js/src/integration-main.js
@@ -1,22 +1,32 @@
 // An integration test
-const path = require("path");
-const TorrentClient = require("./TorrentClient.js");
+const WebTorrent = require("webtorrent");
 
 async function main() {
-  // I seeded a random image torrent with instant.io and this is the magnet url
+  let client = new WebTorrent();
+
   let magnet =
-    "magnet:?xt=urn:btih:4c4b94414ed2cd9b3b2db4c58006610746ceeda8&dn=DrfuEaUX0AUea-5.jpg&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com";
+    "magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent";
 
-  let client = new TorrentClient();
-  console.log("torrent client created");
-  let torrent = await client.download(magnet);
-  console.log("download has begun");
-  await torrent.waitForDone();
-  console.log("download complete");
+  let promise = new Promise((resolve, reject) => {
+    client.add(magnet, torrent => {
+      for (let file of torrent.files) {
+        console.log("found file with name:", file.name);
+      }
 
-  // TODO: check something about the data
+      torrent.on("done", () => {
+        console.log("downloaded", torrent.downloaded, "bytes");
+        console.log("progress:", torrent.progress);
+        client.destroy(err => {
+          if (err) {
+            console.log("error in destruction:", err);
+          }
+          resolve(null);
+        });
+      });
+    });
+  });
 
-  await client.destroy();
+  return await promise;
 }
 
 main()
