af0dfa996cc5b4a095bd75d3ac6f16636910b121
Kevin Lacker <lacker@gmail.com>
2019-03-26 10:42:44 -0700

    seed then dload

--- js/src/TorrentClient.test.js
+++ js/src/TorrentClient.test.js
@@ -8,23 +8,22 @@ test("Creating and shutting down a torrent client", async () => {
 });
 
 test(
-  "Seeding a torrent",
+  "Seeding a torrent, then downloading it",
   async () => {
+    // Seed a torrent
     let seedClient = new TorrentClient();
     let dir = path.resolve(__dirname, "samplesite");
     let t = await seedClient.seed(dir);
+
+    // Download that exact torrent
+    let downloadClient = new TorrentClient();
+    let torrent = await downloadClient.download(t.magnet);
+    await torrent.waitForDone();
+
+    // TODO: check the data is ok
+
     await seedClient.destroy();
+    await downloadClient.destroy();
   },
-  10000
+  30000
 );
-
-/*
-  // Download that exact torrent
-  let downloadClient = new TorrentClient();
-  let torrent = await downloadClient.download(magnet);
-  await torrent.waitForDone();
-
-  // TODO: check that the download works
-});
-
-*/
