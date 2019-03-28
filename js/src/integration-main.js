ceb4f6d300d15c76129737984b61c50b09f3bb27
Kevin Lacker <lacker@gmail.com>
2019-03-28 12:55:03 -0700

    shutdown not working

--- js/src/integration-main.js
+++ js/src/integration-main.js
@@ -2,29 +2,7 @@
 const WebTorrent = require("webtorrent-hybrid");
 const TorrentClient = require("./TorrentClient.js");
 
-const SINTEL =
-  "magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent";
-
-const DAT = "9ca28626647404a6fd831b56fb2e8530891a943e";
-
-/* Works for sintel but that's it
-async function main() {
-  let client = new TorrentClient();
-  client.verbose = true;
-  let torrent = await client.download(SAMPLESITE);
-  await torrent.monitorProgress();
-  client.destroy();
-}
-
-main()
-  .then(() => {
-    console.log("done");
-  })
-  .catch(e => {
-    console.log("Unhandled " + e);
-    process.exit(1);
-  });
-*/
+// TODO: figure out why this doesn't shut down cleanly
 
 const SAMPLESITE =
   "magnet:?xt=urn:btih:e60f82343019bd711c5c731b46e118b0f2b2ecc6&dn=samplesite&tr=ws%3A%2F%2Flocalhost%3A4444&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com";
@@ -40,5 +18,7 @@ client.add(info, torrent => {
 
   torrent.on("done", () => {
     console.log("downloaded", torrent.downloaded, "bytes");
+    console.log("shutting down");
+    torrent.destroy();
   });
 });
