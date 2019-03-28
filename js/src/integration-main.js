b739203a597f69383c8710cdbe5f895e0eb30855
Kevin Lacker <lacker@gmail.com>
2019-03-28 11:20:58 -0700

    fix integration test

--- js/src/integration-main.js
+++ js/src/integration-main.js
@@ -26,7 +26,8 @@ main()
   });
 */
 
-const SAMPLESITE = "e60f82343019bd711c5c731b46e118b0f2b2ecc6";
+const SAMPLESITE =
+  "magnet:?xt=urn:btih:e60f82343019bd711c5c731b46e118b0f2b2ecc6&dn=samplesite&tr=ws%3A%2F%2Flocalhost%3A4444&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com";
 
 let client = new WebTorrent();
 let info = SAMPLESITE;
