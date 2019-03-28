9f08468a335f21af0cd2a9c606f107721ab853fc
Kevin Lacker <lacker@gmail.com>
2019-03-28 11:54:27 -0700

    Tracker object

--- js/src/host-main.js
+++ js/src/host-main.js
@@ -5,39 +5,16 @@
 const http = require("http");
 const path = require("path");
 
-const Tracker = require("bittorrent-tracker");
+// const Tracker = require("bittorrent-tracker");
 const WebTorrent = require("webtorrent-hybrid");
 
 // Run a black hole proxy
 const BlackHoleProxy = require("./BlackHoleProxy.js");
 let proxy = new BlackHoleProxy(3333);
 
-// Run a webtorrent tracker
-// See https://github.com/webtorrent/bittorrent-tracker for docs
-let server = new Tracker.Server({
-  udp: true,
-  http: true,
-  ws: true,
-  stats: true,
-  filter: (infoHash, params, callback) => {
-    // Allow tracking all torrents
-    // TODO: restrict this in a logical way
-    console.log("tracking", infoHash);
-    callback(null);
-  }
-});
-server.on("listening", () => {
-  // fired when all requested servers are listening
-  console.log("tracker listening on http port " + server.http.address().port);
-  console.log("tracker listening on udp port " + server.udp.address().port);
-  console.log(
-    "tracker listening on websocket port " + server.ws.address().port
-  );
-});
-server.on("start", addr => {
-  console.log("got start message from " + addr);
-});
-server.listen(4444, "localhost");
+// Run a tracker
+const Tracker = require("./Tracker.js");
+let tracker = new Tracker(4444);
 
 // TODO: remove this webtorrent seeding once the deploy-based seeding is working
 // Seed a WebTorrent
