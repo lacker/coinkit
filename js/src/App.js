1a835c92277824681aa6ab56bae999e547302b56
Kevin Lacker <lacker@gmail.com>
2019-03-29 10:51:00 -0700

    in-browser downloading works

--- js/src/App.js
+++ js/src/App.js
@@ -8,7 +8,8 @@ import KeyPair from "./KeyPair";
 import TorrentClient from "./TorrentClient";
 
 async function fetchPeerData() {
-  const SAMPLESITE = "e60f82343019bd711c5c731b46e118b0f2b2ecc6";
+  const SAMPLESITE =
+    "magnet:?xt=urn:btih:e60f82343019bd711c5c731b46e118b0f2b2ecc6&dn=samplesite&tr=ws%3A%2F%2Flocalhost%3A4444&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com";
   let client = new TorrentClient();
   client.verbose = true;
   let torrent = await client.download(SAMPLESITE);
