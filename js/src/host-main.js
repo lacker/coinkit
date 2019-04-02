3207c0ca788b8327763dd1914944fbe94011e095
Kevin Lacker <lacker@gmail.com>
2019-04-02 09:34:28 -0700

    host args

--- js/src/host-main.js
+++ js/src/host-main.js
@@ -1,11 +1,19 @@
 // This is the entry point for the hosting server.
 
-// TODO: make this read args and create a HostingServer
+// TODO: make this read args for provider id and directory to store stuff in.
+// TODO: create a HostingServer
 
 const http = require("http");
 const path = require("path");
 
-const WebTorrent = require("webtorrent-hybrid");
+if (process.argv.length != 4) {
+  console.log("usage: npm run host <providerID> <directory for hosting files>");
+  process.exit(1);
+}
+
+let [_, _, id, directory] = process.argv;
+console.log("id:", id);
+console.log("directory:", directory);
 
 // Run a black hole proxy
 const BlackHoleProxy = require("./BlackHoleProxy.js");
