561faed2b8009a1951faa60fba8bafdcf3792b51
Kevin Lacker <lacker@gmail.com>
2019-04-05 11:33:22 -0700

    flaggin

--- js/src/host-main.js
+++ js/src/host-main.js
@@ -1,37 +1,46 @@
 // This is the entry point for the hosting server.
 
-// TODO: make this read args for provider id and directory to store stuff in.
-// TODO: create a HostingServer
-
 const fs = require("fs");
 const http = require("http");
+const os = require("os");
 const path = require("path");
+
+const args = require("args");
+
+args
+  .option("tracker", "The port on which the tracker will be running", 4444)
+  .option("proxy", "The port on which the proxy will be running", 3333)
+  .option("id", "The provider id to host files for", 1)
+  .option(
+    "directory",
+    "The directory to store files in",
+    path.join(os.homedir(), "hostfiles")
+  );
+
+const flags = args.parse(process.argv);
 
 const BlackHoleProxy = require("./BlackHoleProxy.js");
 const HostingServer = require("./HostingServer.js");
 const Tracker = require("./Tracker.js");
 
-if (process.argv.length != 4) {
-  console.log("usage: npm run host <providerID> <directory for hosting files>");
+if (
+  !fs.existsSync(flags.directory) ||
+  !fs.lstatSync(flags.directory).isDirectory()
+) {
+  console.log(flags.directory, "is not a directory");
+  process.exit(1);
+}
+if (flags.id < 1) {
+  console.log(flags.id, "is not a valid provider id");
   process.exit(1);
 }
 
-let [node, hostmainjs, sid, directory] = process.argv;
-let id = parseInt(sid);
-if (id == 0) {
-  console.log("bad id:", sid);
-  process.exit(1);
-}
-if (!fs.existsSync(directory) || !fs.lstatSync(directory).isDirectory()) {
-  console.log(directory, "is not a directory");
-  process.exit(1);
-}
-console.log("hosting files for provider", id, "in", directory);
-let host = new HostingServer(id, directory, true);
+console.log("hosting files for provider", flags.id, "in", flags.directory);
+let host = new HostingServer(flags.id, flags.directory, true);
 host.serve();
 
 // Run a black hole proxy
-let proxy = new BlackHoleProxy(3333);
+let proxy = new BlackHoleProxy(flags.proxy);
 
 // Run a tracker
-let tracker = new Tracker(4444);
+let tracker = new Tracker(flags.tracker);
