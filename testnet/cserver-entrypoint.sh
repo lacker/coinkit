fb3001058c734be9676ad990542cd01329acf8fe
Kevin Lacker <lacker@gmail.com>
2019-04-10 11:12:11 -0700

    make a reboot more obvious

--- testnet/cserver-entrypoint.sh
+++ testnet/cserver-entrypoint.sh
@@ -3,6 +3,8 @@
 # This script is the entry point for the Docker container, designed to be run on
 # the Google cloud platform from the coinkit directory.
 
+echo ------------------------------ cserver-entrypoint.sh ------------------------------
+
 KEYPAIR=`find /secrets/keypair | grep json | head -1`
 echo loading keypair: $KEYPAIR
 
