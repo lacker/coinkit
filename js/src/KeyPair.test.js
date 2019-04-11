d174aae7fce7938e0bc562cb5bd81a593b13d890
Kevin Lacker <lacker@gmail.com>
2019-04-11 11:40:45 -0700

    fix tests

--- js/src/KeyPair.test.js
+++ js/src/KeyPair.test.js
@@ -55,8 +55,8 @@ test("KeyPair can be constructed from a private key", () => {
 
 test("KeyPair specific signatures", () => {
   let serialized = `{
-  "Public": "0x5cb9ad1487197f63a69f5c51c8bc53fefe6f55f7d01e5509dd0ad055d44eff4f9a86",
-  "Private": "1YBC5qpaM14DrVdsap5DtBWRv9IHf3Leyd95MOSSBV1cua0Uhxl/Y6afXFHIvFP+/m9V99AeVQndCtBV1E7/Tw"
+  "public": "0x5cb9ad1487197f63a69f5c51c8bc53fefe6f55f7d01e5509dd0ad055d44eff4f9a86",
+  "private": "1YBC5qpaM14DrVdsap5DtBWRv9IHf3Leyd95MOSSBV1cua0Uhxl/Y6afXFHIvFP+/m9V99AeVQndCtBV1E7/Tw"
 }
 `;
   let kp = KeyPair.fromSerialized(serialized);
