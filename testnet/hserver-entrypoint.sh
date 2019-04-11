3aae2ba63cfa6e47c1347ef7cfb1692c8924f705
Kevin Lacker <lacker@gmail.com>
2019-04-11 09:50:08 -0700

    hserver

--- testnet/hserver-entrypoint.sh
+++ testnet/hserver-entrypoint.sh
@@ -3,11 +3,11 @@
 # This script is the entry point for a Docker container that runs the node hosting server.
 # It is designed to be run on the Google cloud platform from the coinkit/js directory.
 
-echo ------------------------------ host-entrypoint.sh ------------------------------
+echo ------------------------------ hserver-entrypoint.sh ------------------------------
 
 KEYPAIR=`find /secrets/keypair | grep json | head -1`
 echo loading keypair: $KEYPAIR
 
 # TODO: make stuff work
 
-npm run host
+npm run hserver
