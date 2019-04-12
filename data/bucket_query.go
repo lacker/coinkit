a10a8595c70eaec21b119081c7019497c25ca2c8
Kevin Lacker <lacker@gmail.com>
2019-04-12 10:23:54 -0700

    fix bucket query display

--- data/bucket_query.go
+++ data/bucket_query.go
@@ -9,8 +9,8 @@ type BucketQuery struct {
 	Name     string   `json:"name"`
 	Names    []string `json:"names"`
 	Owner    string   `json:"owner"`
-	Limit    int      `json:"limit"`
 	Provider uint64   `json:"provider"`
+	Limit    int      `json:"limit"`
 }
 
 func (q *BucketQuery) String() string {
@@ -18,11 +18,17 @@ func (q *BucketQuery) String() string {
 	if q.Name != "" {
 		parts = append(parts, fmt.Sprintf("name=%s", q.Name))
 	}
+	if len(q.Names) > 0 {
+		parts = append(parts, strings.Join(q.Names, ","))
+	}
 	if q.Owner != "" {
 		parts = append(parts, fmt.Sprintf("owner=%s", q.Owner))
 	}
 	if q.Provider != 0 {
 		parts = append(parts, fmt.Sprintf("provider=%d", q.Provider))
+	}
+	if q.Limit != 0 {
+		parts = append(parts, fmt.Sprintf("limit=%d", q.Limit))
 	}
 	if len(parts) == 0 {
 		return "<empty>"
