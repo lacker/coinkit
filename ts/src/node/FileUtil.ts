067f7ac7166b15f9ea339e50a80cdc8434533315
Kevin Lacker <lacker@gmail.com>
2019-04-22 10:21:26 -0700

    keypair needs to take symbolic links

--- ts/src/node/FileUtil.ts
+++ ts/src/node/FileUtil.ts
@@ -17,16 +17,9 @@ export function loadKeyPair(filename) {
     throw new Error(filename + " does not exist");
   }
   let stat = fs.lstatSync(filename);
-  if (!stat.isFile()) {
-    console.log("XXX stat:", stat);
-    console.log("XXX stat.isFile() ", stat.isFile());
-    console.log("XXX stat.isDirectory() ", stat.isDirectory());
-    console.log("XXX stat.isBlockDevice() ", stat.isBlockDevice());
-    console.log("XXX stat.isSymbolicLink() ", stat.isSymbolicLink());
-    console.log("XXX stat.isCharacterDevice() ", stat.isCharacterDevice());
-    console.log("XXX stat.isFIFO() ", stat.isFIFO());
-    console.log("XXX stat.isSocket() ", stat.isSocket());
-    throw new Error(filename + " isFile is false");
+  if (!stat.isFile() && !stat.isSymbolicLink()) {
+    console.log(filename, "lstat:", stat);
+    throw new Error("cannot read " + filename);
   }
   if (!isFile(filename)) {
     throw new Error(filename + " is not a file");
