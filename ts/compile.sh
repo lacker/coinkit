16f51d97c9dd53c5077fa6bb0bdc9e553bac585e
Kevin Lacker <lacker@gmail.com>
2019-05-02 11:38:52 -0700

    better compile script

--- ts/compile.sh
+++ ts/compile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo tsc --build tsconfig.node.json
 tsc --build tsconfig.node.json || exit 1
-echo node compile ok
+echo OK
+echo tsc --build tsconfig.browser.json
 tsc --build tsconfig.browser.json || exit 1
-echo browser compile ok
+echo OK
